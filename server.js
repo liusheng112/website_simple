1721b9e0be830c24d9fd0dc7aba7651faa4ee31c
liusheng112 <361707502@qq.com>
2018-11-23 18:10:49 +0800

    remove express favicon middleware

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const compression = require('compression');
 const app = express();
 const port = 8888;
 const projectRootPath = path.join(__dirname, 'static', 'dist');
 
 app.use(compression());
-app.use(favicon(path.join(__dirname, 'static', 'favicon.ico')));
 app.use(express.static(projectRootPath));
 app.use((req, res) => {
     res.sendFile(projectRootPath + '/index.html');
